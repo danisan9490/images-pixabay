de65ce19dce9a4597dba24faca5706aede820054
Daniel <danisan9490@gmail.com>
2020-10-25 16:09:47 +0000

    #4 Show Image Info

--- client/src/components/Imagen.js
+++ client/src/components/Imagen.js
@@ -9,6 +9,19 @@ const Imagen = ({ imagen }) => {
     <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
       <div className="card">
         <img src={previewURL} alt={tags} className="card-img-top" />
+        <div className="card-body">
+          <p className="card-text">{likes} Like</p>
+          <p className="card-text">{views} Views</p>
+        </div>
+
+        <div className="card-footer">
+          <a
+            href={largeImageURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-primary btn-block"
+          >Open Image</a>
+        </div>
       </div>
     </div>
   );
