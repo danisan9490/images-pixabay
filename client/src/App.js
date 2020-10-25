6a22827abc1db271435fae66f2df42072d5964c0
Daniel <danisan9490@gmail.com>
2020-10-25 15:50:54 +0000

    #1 Create App and Form

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Formulario from './components/Formulario';
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <p className="lead text-center">Search Images</p>
+
+        <Formulario />
+      </div>
+    </div>
+  );
+}
+
+export default App;
