0548cb343205f289c57936bcfbbe17de305c4a8d
Daniel <danisan9490@gmail.com>
2020-10-25 16:07:55 +0000

    #3 Get and Add img

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
-
+import ListadoImagenes from './components/ListadoImagenes';
 
 function App() {
+  // state de la app
+  const [busqueda, guardarBusqueda] = useState('');
+  const [imagenes, guardarImagenes] = useState([]);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      if (busqueda === '') return;
+
+      const imagenesPorPagina = 10;
+      const key = '1732750-d45b5378879d1e877cd1d35a6';
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+
+      guardarBusqueda(resultado.hits);
+      guardarImagenes(resultado.hits);
+    }
+    consultarApi();
+  }, [busqueda])
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Search Images</p>
 
-        <Formulario />
+        <Formulario
+          guardarBusqueda={guardarBusqueda}
+        />
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes
+          imagenes={imagenes}
+        />
       </div>
     </div>
   );
